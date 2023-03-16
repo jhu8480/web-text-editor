cc1f068af8109a1c7eb74d96579c07fbed5a8493
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-16 19:56:27 -0400

    working on download feature

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,5 +16,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('😇', 'appinstalled', event);
+    console.log('appinstalled', event);
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// TODO: Configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -29,11 +30,11 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Text Editor",
-        short_name: "J.A.T.E",
-        description: "This is an offline text editor",
-        background_color: "#cfcfcf",
-        theme_color: "#cfcfcf",
+        name: "Jate",
+        short_name: "Jate",
+        description: "text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
         icons: [
@@ -45,20 +46,27 @@ module.exports = () => {
         ],
       }),
     ],
-
     module: {
       rules: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
         {
-          test: /\.js$/,
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
